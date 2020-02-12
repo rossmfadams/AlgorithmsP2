cb1968a4c4526e5ed6708a8f46b799aa89eab270
rossmfadams <55807361+rossmfadams@users.noreply.github.com>
2020-02-12 16:37:35 -0600

    Create source.py

---
+++ source.py
@@ -0,0 +1,8 @@
+##  Source.py for Algorithms Project 2
+##
+##  A test suite for analyzing the performance of sorting algorithms in the best, 
+##  most likely, and worst cases.
+##
+##  @author Ross Adams, Longtin Hang, and Riley Williams
+
+## Be sure to comment your code!
